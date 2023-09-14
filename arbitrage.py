e4c6bb83c79e2372f07b16d86aa69489cf99572e
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-14 13:37:02 -0700

    Create arbitrage.py

---
+++ arbitrage.py
@@ -0,0 +1,21 @@
+#import requests
+import json
+import random
+exchanges = ["Binance", "Coinbase"]
+tokens = ["Eth", "Bitcoin", "XTZ"]
+
+
+def scan_crypto_prices():
+    """ Scans for updated crypto prices and saves the relevant data."""
+    pass
+
+def paper_training():
+    """ Tests the current model in a simulated real life environment to compare performance."""
+    pass
+
+def build_arb_model():
+    """ Builds a new arbitrage model to test."""
+    pass
+
+def optimize_neural_network():
+    pass
