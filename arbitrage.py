df432facdac235a3c5b0841800ef64f1ffb49611
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-15 21:17:09 -0700

    Added multi-library support for arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -9,14 +9,17 @@ import os
 # ETHEREUM_API = os.environ["eth_api"]
 # BITCOIN_API = os.environ["btc_api"]
 crypto_tokens = ["Matic", "Sol", "Eth", "Btc"]
+ml_tools = ["keras", "tensorflow", "skLearn", "pytorch"]
 
 class arb_batch():
     """ An object consisting of arbitrage models to use for trading assets."""
     
-    def __init__(self, tokens):
+    def __init__(self, tokens, tools):
         self.models = {"Crypto": {}}
-        for token in tokens:
-            self.models["Crypto"][token] = {"Best": None, "Challenger": None, "Test": None, "Control": None}
+        for tool in tools:
+            self.models["Crypto"][tool] = {}  # Create a dictionary for each ML tool
+            for token in tokens:
+                self.models["Crypto"][tool][token] = {"Best": None, "Challenger": None, "Test": None, "Control": None}
 
 current_best_models = []
 
@@ -60,21 +63,24 @@ def paper_training(asset_type):
     """ Tests the current model in a simulated real-life environment to compare performance."""
     pass
 
-def build_arb_batch(asset_lists):
+def build_arb_batch(asset_lists, ml_tool_list):
     """ Builds arbitrage model batches to test. Should test exchange-based arbitrage, triangular arbitrage (USD -> ETH -> BTC), and variations of hybrids."""
     print("Need to check for a list of existing models to build/test against.")
     if len(current_best_models) == 0:
-        new_batch = arb_batch(asset_lists)
-        print(new_batch.models)
+        new_batch = arb_batch(asset_lists, ml_tool_list)
+        #print(new_batch.models)
         print("Blank batch created, populating models for each asset.")
-    for asset in new_batch.models:
-        for token in new_batch.models[asset]:
-              for model in new_batch.models[asset][token]:
-                  if new_batch.models[asset][token][model] == None:
-                      print("Can't have empty models, need to create random models to begin.")
-                      print(f"{token} {model} Model:", new_batch.models[asset][token][model])
-                      new_model = generate_new_model()
-                      new_batch.models[asset][token][model] = new_model
+    for tool in ml_tools:
+        for asset in new_batch.models:
+            for token in new_batch.models[asset][tool]:
+                for model in new_batch.models[asset][tool][token]:
+                    if new_batch.models[asset][tool][token][model] is not None:
+                        print("model exists")
+                    else:
+                        print("Can't have empty models, need to create random models to begin.")
+                        print(f"{token} {tool} Model:", new_batch.models[asset][tool][token])
+                        new_model = generate_new_model(tool, token)
+                        new_batch.models[asset][tool][token][model] = new_model
     print(new_batch.models)
     # Check for previous data to try to inherit working methods.
     # Want to test models against each other in ways that can help us reverse engineer performance increases.
@@ -88,15 +94,13 @@ def build_arb_batch(asset_lists):
     # 10 epochs, 100 epochs, 1000 epochs, 10000 epochs. Each with different tests and mutation schedules.
     # Will probably do batches to reduce errors/ remove crazy rng.
 
-
-
-def generate_new_model():
+def generate_new_model(ml_tool, coin):
     """ Generates a new neural network arbitrage model and saves it to the arb batch."""
     # Not entirely sure how to finish this...
     # May use an evolutionary style cnn that can pass on traits/mutations, and use randomness to retry different evolutionary paths...
+    print(f"Generating {ml_tool} {coin} model.")
     new_model = 1
     return new_model
-
 
 def arbitrage_crypto(coin_type):
     pass
