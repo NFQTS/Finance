7d802af6e215de3356d1aab69fdba751f420256b
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-17 18:24:11 -0700

    upgraded json handling binanceus_arb.py

--- binanceus_arb.py
+++ binanceus_arb.py
@@ -4,12 +4,12 @@ import json
 
 # Define the cryptocurrency pairs you want to monitor
 pairs = ['BTC/USDT', 'BTC/USDC']
-wallet_total = {"USDT": 100, "USDC": 100, "BTC":.0002}
-risk_percentage = .005
+wallet_total = {"USDT": 100, "USDC": 100, "BTC": 0.0002}
+risk_percentage = 0.005
 
 # Define criteria for data management
 MAX_RUNTIME_SECONDS = 60  # Maximum runtime in seconds (1 hour)
-
+order_depth = 15
 def load_exchange_orders():
     try:
         with open('exchange_order_data.json', 'r') as json_file:
@@ -21,19 +21,27 @@ def load_exchange_orders():
 exchange_order_dict = load_exchange_orders()
 highest_arb_opp = 0
 
+
+# Function to save exchange orders to JSON
+def save_exchange_orders():
+    # Serialize the exchange_order_dict to JSON and write it to a file
+    with open('exchange_order_data.json', 'w') as json_file:
+        json.dump(exchange_order_dict, json_file, indent=4)
+    print("Exchange order data saved to 'exchange_order_data.json'.")
+
 def detect_arbitrage_opportunity(pair):
     try:
         # Fetch order book data
         exchange_binanceus = ccxt.binanceus()
         order_book = exchange_binanceus.fetch_order_book(pair)
         store_exchange_orders(exchange_binanceus.id, order_book, pair)
-
+        #print(order_book)
         # Calculate potential profit percentage
-        bid_price = order_book['bids'][0][0]
-        ask_price = order_book['asks'][0][0]
+        bid_price = order_book['bids'][1][0]
+        ask_price = order_book['asks'][1][0]
         profit_percentage = ((ask_price - bid_price) / bid_price) * 100
 
-        if profit_percentage > .1:  # Arbitrage opportunity threshold
+        if profit_percentage > 0.1:  # Arbitrage opportunity threshold
             # Print arbitrage opportunity details
             print(f"Arbitrage Opportunity Detected for {pair} on {exchange_binanceus.id}!")
             print(f"Buy {pair} at {bid_price}")
@@ -61,27 +69,21 @@ def store_exchange_orders(exchange, data, asset_pair):
     if asset_pair not in exchange_order_dict[exchange_str]:
         exchange_order_dict[exchange_str][asset_pair] = []
 
-    # Extract best bid and ask prices
-    bid_price = data['bids']
-    ask_price = data['asks']
+    # Extract the first 15 bids and asks
+    bids = data['bids'][:order_depth]
+    asks = data['asks'][:order_depth]
 
     # Store the order book data for the specific pair along with a timestamp
     timestamp = int(time.time())  # Get the current Unix timestamp
     exchange_order_dict[exchange_str][asset_pair].append({
         timestamp: {
-            'bid_price': bid_price,
-            'ask_price': ask_price
+            'bids': bids,
+            'asks': asks
         }
     })
 
     print("Records updated.")
-
-def save_exchange_orders():
-    # Serialize the exchange_order_dict to JSON and write it to a file
-    with open('exchange_order_data.json', 'w') as json_file:
-        json.dump(exchange_order_dict, json_file, indent=4)
-    print("Exchange order data saved to 'exchange_order_data.json'.")
-
+    save_exchange_orders()
 # Main loop
 run = True
 while run:
@@ -89,9 +91,6 @@ while run:
     # Check for arbitrage opportunities for each pair
     for pair in pairs:
         detect_arbitrage_opportunity(pair)
-
-    # Save the stored order book data with timestamps as a JSON file
-    save_exchange_orders()
 
     # Check the runtime
     current_time = time.time()
@@ -102,10 +101,14 @@ while run:
         break
 
     print(f"Highest Profit Percentage Found: {highest_arb_opp:.2f}%")
-    print("Sleeping for 60 seconds to give the market time to move.")
-    time.sleep(60)
+    print("Sleeping for a few seconds to give the market time to move.")
+    time.sleep(5)
 
-
+trading_rules = {
+    "profit_threshold": .25,
+    "loss_tolerance": risk_percentage,
+    "base_bet_USDT": 100
+}
 
 
 # Need to make a way for reinforcement learning agents to compete for profits.
